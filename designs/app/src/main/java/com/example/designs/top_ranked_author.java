d4fc70fc68ea599ec782692a19805bf92c2ad61e
Njeri1 <43375850+Njeri1@users.noreply.github.com>
2020-05-21 01:12:09 +0300

    Add files via upload

---
+++ designs/app/src/main/java/com/example/designs/top_ranked_author.java
@@ -0,0 +1,14 @@
+package com.example.designs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class top_ranked_author extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_ranked_author);
+    }
+}
